64d407b5e13ff78dfc4e3e576cde917ec9474d35
davidlm-dev <david03lm@gmail.com>
2025-09-03 15:19:53 +0200

    Base server running with eslint/prettier with precommit

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,9 @@ const app = express()
 const PORT = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hola Prettier!')
+  res.send('Servidor Express básico corriendo! ')
 })
 
-console.log(`Servidor corriendo en http://localhost:${PORT}`)
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`)
+})
